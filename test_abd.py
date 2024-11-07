4b44fa283955de8663756ffd3f629b80aa352af2
hammad-kamran <158271960+hammad-kamran@users.noreply.github.com>
2024-11-07 12:04:35 +0500

    Update test_abd.py

--- test_abd.py
+++ test_abd.py
@@ -21,6 +21,7 @@ def my_cred():
     return [
     ('standard_user', 'secret_sauce'),
     ('locked_out_user', 'secret_sauce'),
+    ('problem_user', 'secret_sauce'),
     ('performance_glitch_user', 'secret_sauce')
     ]
     
